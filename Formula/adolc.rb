68d59dbc130dfa86ca45ae44f677aaa6f4a48ccf
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-07-12 22:35:44 -0300

    :sparkles: Add formulas for ADOL-C and ColPack

---
+++ Formula/adolc.rb
@@ -0,0 +1,40 @@
+class Adolc < Formula
+  desc "Package for Automatic Differentiation of Algorithms Written in C/C++"
+  homepage "https://github.com/coin-or/ADOL-C"
+  url "https://github.com/coin-or/ADOL-C/archive/refs/tags/releases/2.7.2.tar.gz"
+  sha256 "701e0856baae91b98397960d5e0a87a549988de9d4002d0e9a56fa08f5455f6e"
+
+  depends_on "col-pack"
+  depends_on "gcc" => :build
+
+  def install
+    gcc = Formula["gcc"]
+    ENV["CC"]  = "/usr/local/bin/gcc-#{gcc.version_suffix}"
+    ENV["LD"]  = "/usr/local/bin/gcc-#{gcc.version_suffix}"
+    ENV["CXX"] = "/usr/local/bin/g++-#{gcc.version_suffix}"
+
+    colpack = Formula["col-pack"]
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--libdir=#{prefix}/lib",
+                          "--with-openmp-flag=-fopenmp",
+                          "--with-colpack=#{colpack.prefix}/",
+                          "--enable-sparse",
+                          "--enable-docexa",
+                          "--enable-addexa",
+                          "--enable-parexa"
+    system "make", "install"
+
+    prefix.install "ADOL-C/examples"
+  end
+
+  test do
+    chdir prefix do
+      system "true"
+      # system "./examples/detexam <<< 4"
+      # system "./examples/odexam <<< 4"
+      # system "./examples/powexam <<< 10"
+      # system "./examples/speelpenning <<< 10"
+    end
+  end
+end
---
+++ Formula/col-pack.rb
@@ -0,0 +1,35 @@
+# typed: false
+# frozen_string_literal: true
+
+# A graph coloring algorithm package.
+class ColPack < Formula
+  desc "Graph Coloring Algorithm Package"
+  homepage "https://github.com/CSCsw/ColPack"
+  url "https://github.com/CSCsw/ColPack/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "b22ead7da80fa1735291b2d83198adf41bf36101e4fcb2c4f07c1cfacf211c75"
+  license "BSD-3-Clause"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gcc" => :build
+  depends_on "libtool" => :build
+
+  def install
+    gcc = Formula["gcc"]
+    ENV["CC"]  = "/usr/local/bin/gcc-#{gcc.version_suffix}"
+    ENV["LD"]  = "/usr/local/bin/gcc-#{gcc.version_suffix}"
+    ENV["CXX"] = "/usr/local/bin/g++-#{gcc.version_suffix}"
+
+    system "autoreconf", "-vif"
+    system "./configure", "--prefix=#{prefix}", "--enable-examples"
+    system "make", "install"
+
+    prefix.install "Graphs"
+  end
+
+  test do
+    chdir prefix do
+      system "./examples/Basic/color_graph_using_GraphColoringInterface"
+    end
+  end
+end
